ace6d2ddccb1f6f7695cf515a0960c778f663414
gloton <gloton@gmail.com>
2013-01-21 13:03:32 -0300

    Publicacion inicial

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Calendario estilizado con CSS - CSSBlog ES</title>
+	<link href="http://www.cssblog.es/ejemplos/calendario-css/styles2.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h1>Calendario estilizado con CSS - CSSBlog ES</h1>
+	
+	<table id="calendar" summary="This month's calendar" cellpadding="0" cellspacing="0">
+	<caption><a href="#" title="previous month" class="nav">«</a> Marzo <a href="#" title="next month" class="nav">»</a></caption>
+	
+	 <tbody><tr>
+		<th scope="col" abbr="Sunday" title="Sunday">S</th>
+		<th scope="col" abbr="Monday" title="Monday">M</th>
+		<th scope="col" abbr="Tuesday" title="Tuesday">T</th>
+		<th scope="col" abbr="Wednesday" title="Wednesday">W</th>
+		<th scope="col" abbr="Thursday" title="Thursday">T</th>
+		<th scope="col" abbr="Friday" title="Friday">F</th>
+		<th scope="col" abbr="Saturday" title="Saturday">S</th>
+	 </tr>
+	 <tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>1</td>
+		<td>2</td>
+		<td>3</td>
+		<td>4</td>
+		<td>5</td>
+	 </tr>
+	 <tr>
+		<td class="today"><a href="#">6</a></td>
+		<td>7</td>
+		<td>8</td>
+		<td>9</td>
+		<td><a href="#">10</a></td>
+		<td>11</td>
+		<td>12</td>
+	 </tr>
+	 <tr>
+		<td><a href="#">13</a></td>
+		<td>14</td>
+		<td>15</td>
+		<td>16</td>
+		<td>17</td>
+		<td>18</td>
+		<td>19</td>
+	 </tr>
+	 <tr>
+		<td>20</td>
+		<td>21</td>
+		<td>22</td>
+		<td><a href="#">23</a></td>
+		<td>24</td>
+		<td>25</td>
+		<td>26</td>
+	 </tr>
+	 <tr>
+		<td>27</td>
+		<td>28</td>
+		<td>29</td>
+		<td>30</td>
+		<td>31</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+	 </tr>
+	</tbody></table>
+	
+	<p><a href="http://www.cssblog.es/ejemplos/calendario-css/calendario-css.zip" title="Descargar archivo .ZIP del ejemplo">&#8594; Descargar archivo .ZIP del ejemplo</a></p>
+	
+	<p><a href="http://www.cssblog.es/calendario-estilizado-con-css/" title="Volver al artículo en CSSBlog ES">&#8594; Volver al artículo en CSSBlog ES</a></p>
+	</body>
+</html>
